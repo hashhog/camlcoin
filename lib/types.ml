f0ea92a72b96e39e0ba2df6931489ed28ca276a2
Max <maxtclinton@gmail.com>
2026-03-08 22:13:21 -0400

    add types module with version constants and library interface

--- lib/types.ml
+++ lib/types.ml
@@ -1,8 +1,2 @@
-type block_header = {
-  version : int;
-  prev_hash : bytes;
-  merkle_root : bytes;
-  timestamp : int;
-  bits : int;
-  nonce : int;
-}
+let version = "0.1.0"
+let protocol_version = 70016l

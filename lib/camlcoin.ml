134f8923eca03bbdd3fa4d97f8cdb38d5ff51ba0
max <maxtclinton@gmail.com>
2026-03-14 03:37:17 -0400

    expose rest module in library interface

--- lib/camlcoin.ml
+++ lib/camlcoin.ml
@@ -15,6 +15,7 @@ module Mempool = Mempool
 module Fee_estimation = Fee_estimation
 module Mining = Mining
 module Rpc = Rpc
+module Rest = Rest
 module Bip39 = Bip39
 module Wallet = Wallet
 module Cli = Cli
